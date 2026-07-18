87a189fbdc8466fdf389ea87a20df9333dea58e1
roydenteh99 <roydenteh100@gmail.com>
2026-07-18 20:05:17 +0800

    halfway  to build test for queue manager

--- QueueManager.test.js
+++ QueueManager.test.js
@@ -4,6 +4,9 @@ import { EventQueue } from './QueueManager.js';
 import { Drone } from './Drone.js';
 
 
+const drone = new Drone("drone8", { coolDownTime: 0.1, maxFlightDuration: 1 });
+const battery = new Battery("battery6", { maxFlightTime: 1, chargeTime: 1, chargePercent: 100 });
+
 test('EventQueue droneSourceBatt should not throw when no suitable battery is available', () => {
   const queue = new EventQueue(
     '2026-01-01 09:00',
@@ -16,3 +19,4 @@ test('EventQueue droneSourceBatt should not throw when no suitable battery is av
   const drone = queue.droneList[0];
   assert.doesNotThrow(() => queue.droneSourceBatt(drone));
 });
+
